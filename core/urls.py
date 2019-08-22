a0af2fb48c25d00abd4b87c3ac7aae1ce2560be7
Rafael Marinho <rafamola@gmail.com>
2019-08-22 17:12:15 -0300

    Add all templates of models

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path, include
-from .views import home
+from .views import (
+    home,
+    lista_pessoas,
+    lista_veiculos,
+    lista_movrotativos,
+    lista_mensalistas,
+    lista_movmensalistas
+)
 
 urlpatterns = [
     path('', home, name='core_home'),
+    path('pessoas', lista_pessoas, name='core_lista_pessoas'),
+    path('veiculos', lista_veiculos, name='core_lista_veiculos'),
+    path('movrotativos', lista_movrotativos, name='core_lista_movrotativos'),
+    path('mensalistas', lista_mensalistas, name='core_lista_mensalistas'),
+    path('movmensalistas', lista_movmensalistas, name='core_lista_movmensalistas'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
+from .models import (
+    Pessoa,
+    Veiculo,
+    MovRotativo,
+    Mensalista,
+    MovMensalista
+)
 
 
 def home(request):
     context = {'mensagem': 'Ola munda'}
     return render(request, 'core/index.html', context)
+
+
+def lista_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas})
+
+
+def lista_veiculos(request):
+    veiculos = Veiculo.objects.all()
+    return render(request, 'core/lista_veiculos.html', {'veiculos': veiculos})
+
+
+def lista_movrotativos(request):
+    movRotativos = MovRotativo.objects.all()
+    return render(request, 'core/lista_movrotativos.html', {'movRotativos': movRotativos})
+
+
+def lista_mensalistas(request):
+    mensalistas = Mensalista.objects.all()
+    return render(request, 'core/lista_mensalistas.html', {'mensalistas': mensalistas})
+
+
+def lista_movmensalistas(request):
+    movmensalistas = MovMensalista.objects.all()
+    return render(request, 'core/lista_movmensalistas.html', {'movmensalistas': movmensalistas})
