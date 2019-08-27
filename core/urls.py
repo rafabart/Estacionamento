1ba2e856515b691effd3f47b582ef399e15dd02d
Rafael Marinho <rafamola@gmail.com>
2019-08-26 22:59:37 -0300

    Add insert form Pessoas

--- core/urls.py
+++ core/urls.py
@@ -5,12 +5,14 @@ from .views import (
     lista_veiculos,
     lista_movrotativos,
     lista_mensalistas,
-    lista_movmensalistas
+    lista_movmensalistas,
+    pessoa_novo
 )
 
 urlpatterns = [
     path('', home, name='core_home'),
     path('pessoas', lista_pessoas, name='core_lista_pessoas'),
+    path('pessoas-novo', pessoa_novo, name='core_pessoa_novo'),
     path('veiculos', lista_veiculos, name='core_lista_veiculos'),
     path('movrotativos', lista_movrotativos, name='core_lista_movrotativos'),
     path('mensalistas', lista_mensalistas, name='core_lista_mensalistas'),
